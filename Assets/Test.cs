390100bee2364bdf4d3234ddd9539b4cb30b804b
中西　啓介 <rse43171@nifty.com>
2016-10-11 00:09:52 +0900

    新規作成
    Lesson4の課題の新規作成

---
+++ Assets/Test.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        int[] array = {3124,23424,5451234,134,4125,51,5451};
+
+        for (int i = array.Length - 1; i >= 0;i--) {
+
+
+            Debug.Log(array[i]);
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+	
+	}
+}
