11376ce0fe04b48728ef9da0f80f9d4cd9315bef
中西　啓介 <rse43171@nifty.com>
2016-10-11 21:13:19 +0900

    指摘対応
    レビュー指摘を修正しました。

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -6,10 +6,15 @@ public class Test : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 
-        int[] array = {3124,23424,5451234,134,4125,51,5451};
+        int[] array = {11,22,33,44,55};
+
+        for (int i = 0; i < array.Length; i++){
+
+            Debug.Log(array[i]);
+
+        }
 
         for (int i = array.Length - 1; i >= 0;i--) {
-
 
             Debug.Log(array[i]);
 
